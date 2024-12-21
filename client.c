0f86641305d397bc36b8a73d391b9169e055d4ce
Julio Max Calderone Da Silva <julcalde@1-E-13.42heilbronn.de>
2024-12-21 13:41:00 +0100

    Save before tests

--- client.c
+++ client.c
@@ -6,11 +6,48 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 16:14:55 by julcalde          #+#    #+#             */
-/*   Updated: 2024/12/20 19:20:43 by julcalde         ###   ########.fr       */
+/*   Updated: 2024/12/21 13:33:06 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include <signal.h>
+#include "./libft/include/printf/ft_printf.h"
+#include "./libft.h"
+
+void	send_bit(int pid, char *str)
 {
+	size_t	byte_arr_i;
+	int		i;
+
+	i = 0;
+	while (i < ft_strlen(str))
+	{
+		byte_arr_i = 0;
+		while (byte_arr_i <= 7)
+		{
+			if ((str[i] >> byte_arr_i) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			byte_arr_i++;
+			usleep(100);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		send_bit(pid, argv[2]);
+	}
+	else
+	{
+		ft_printf("Please enter only one word");
+	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,11 +6,44 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 16:14:57 by julcalde          #+#    #+#             */
-/*   Updated: 2024/12/20 19:52:54 by julcalde         ###   ########.fr       */
+/*   Updated: 2024/12/21 13:39:39 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include <signal.h>
+#include "./libft/include/printf/ft_printf.h"
+#include "./libft.h"
+
+void	bits_to_char(int bit)
 {
+	static unsigned char	c;
+	static int				i;
+
+	c = 0;
+	i = 0;
+	if (bit == SIGUSR1)
+		c |= (0 << i);
+	else if (bit == SIGUSR2)
+		c |= (1 << i);
+	i++;
+	if (i == 8)
+	{
+		write (1, &c, 1);
+		if (c == '\0')
+			write (1, '\n', 1);
+		c = 0;
+		i = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("Login to Server %d was successful.\n", getpid());
+	while (1)
+	{
+		signal(SIGUSR2, bits_to_char);
+		signal(SIGUSR1, bits_to_char);
+		pause();
+	}
 	return (0);
 }
